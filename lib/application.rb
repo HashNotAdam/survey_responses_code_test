faeecfa62e143fbd2831d7461ae46361bd1b9108
Adam Rice <development@hashnotadam.com>
2021-02-23 15:21:58 +1100

    feat: CLI to parse and log surveys

--- lib/application.rb
+++ lib/application.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+require "interactions/parse/questions"
+require "interactions/parse/responses"
+require "interactions/log/participation"
+require "interactions/log/rating_question_averages"
+
 class Application
   def cli
-    # Your implementation probably starts here...
+    Process.setproctitle("Culture Amp Survey Response Parser")
+
+    questions = Parse::Questions.run!(data_path: ARGV[0])
+    responses = Parse::Responses.
+      run!(questions: questions, responses_path: ARGV[1])
+    Log::Participation.run!(responses: responses)
+    Log::RatingQuestionAverages.run!(questions: questions, responses: responses)
   end
 end
